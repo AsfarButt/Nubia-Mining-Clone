26f77f15b3d0c71733dd7b1978a9f1225e1981ef
AsfarButt <asfarbutt69@gmail.com>
2025-11-21 16:21:47 +0500

    Update 6

--- app/Home/corevalues.tsx
+++ app/Home/corevalues.tsx
@@ -29,7 +29,7 @@ export default function CoreValues(){
     },[])
     
     return(<div className="relative w-full pb-12 h-fit flex items-start justify-center flex-row flex-wrap bg-white">
-                <h1 className="relative w-full py-4 text-[36px] font-sans font-bold text-center underline decoration-red-600/80">Core Values</h1>
+                <h1 className="relative w-full py-4 text-[36px] text-black font-sans font-bold text-center underline decoration-red-600/80">Core Values</h1>
                 <div className={`relative w-full h-full max-w-120 transition-all delay-50 translate-y-30 opacity-0 duration-800 ease-out p-4 rounded-md border m-4 border-black/20 shadow-md shadow-transparent hover:shadow-black/30`} ref={element1}>
                     <h1 className="w-full text-center text-yellow-800/90 text-[24px] font-semibold font-sans">Responsible Development & Innovation</h1>
                     <p className="text-[17px] px-4 mt-4 pb-8 text-center text-blue-950/85">We pursue advanced exploration and mining using cutting-edge, low impact technologies aligned with JORC principles, ensuring efficiency, safety , and envirnomental reponsibility</p>
