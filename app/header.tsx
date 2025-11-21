d47a113bf1b32f8c822efadc74f30e5cba414133
AsfarButt <asfarbutt69@gmail.com>
2025-11-21 22:11:35 +0500

    Update 17

--- app/header.tsx
+++ app/header.tsx
@@ -15,13 +15,13 @@ export default function Header(){
         <div className="relative w-auto h-full flex flex-row items-center justify-center gap-2 scale-80 sm:scale-100">
             {/* <div className="relative h-full w-13 scale-85 rotate-y-180 bg-white mask-[url(/pickaxe.png)] mask-cover mask-center" />
             <h1 className={`relative text-2xl text-white ${cormorant.className}`}>Nubia Mining</h1> */}
-            <div className="relative w-30 sm:w-32 h-28 mt-2 bg-[url('/brandtext.png')] bg-center bg-cover"></div>
+            <div className="relative w-28 sm:w-32 h-28 mt-2 bg-[url('/brandtext.png')] bg-center bg-cover"></div>
         </div>
         <div className="relative w-[85%] md:w-[30%] h-full flex flex-row items-center justify-between z-2">
-            <a className="relative w-full h-full list-none text-gray-200/90 flex items-center font-sans hover:text-yellow-500/85 justify-center px-1" href="/Home">Home</a>
+            <a className="relative w-full h-full list-none text-gray-200/90 flex items-center text-[15px] sm:text-[17px] font-sans hover:text-yellow-500/85 justify-center sm:px-1" href="/Home">Home</a>
             <div className="w-1.5 h-[85%] bg-white/60" ></div>
-            <a className="relative w-full h-full list-none text-gray-200/90 flex items-center font-sans hover:text-yellow-500/85 justify-center px-1 group" href="/About">About us
-            <div className="absolute top-14 p-2 border-l border-white/80 left-0 h-auto w-auto min-w-35 bg-blue-900/80 hidden group-hover:block pointer-events-auto z-1">
+            <a className="relative w-full h-full list-none text-gray-200/90 flex items-center text-[15px] sm:text-[17px] font-sans hover:text-yellow-500/85 justify-center sm:px-1 group" href="/About">About us
+            <div className="absolute top-14 p-2 border-l border-white/80 left-0 h-auto w-auto min-w-35 bg-blue-900/95 hidden group-hover:block pointer-events-auto z-1">
             <><li className="text-white/65 p-1 hover:text-white/75 hover:underline decoration-white/70">About Nubia</li>
             <li className="text-white/65 p-1 hover:text-white/75 hover:underline decoration-white/70">Mission</li>
             <li className="text-white/65 p-1 hover:text-white/75 hover:underline decoration-white/70">Logistics</li>
@@ -29,9 +29,9 @@ export default function Header(){
             </div>
             </a>
             <div className="w-1.5 h-[85%] bg-white/60" ></div>
-            <a className="relative w-full h-full list-none text-gray-200/90 flex items-center font-sans hover:text-yellow-500/85 justify-center px-1" href="/Products">Products</a>
+            <a className="relative w-full h-full list-none text-gray-200/90 flex items-center text-[15px] sm:text-[17px] font-sans hover:text-yellow-500/85 justify-center sm:px-1" href="/Products">Products</a>
             <div className="w-1.5 h-[85%] bg-white/60" ></div>
-            <a className="relative w-full h-full list-none text-gray-200/90 flex items-center font-sans hover:text-yellow-500/85 justify-center px-1" href="/Services">Services</a>
+            <a className="relative w-full h-full list-none text-gray-200/90 flex items-center text-[15px] sm:text-[17px] font-sans hover:text-yellow-500/85 justify-center sm:px-1" href="/Services">Services</a>
             <div className="w-1.5 h-[85%] bg-white/60" ></div>
         </div>
     </div>)
