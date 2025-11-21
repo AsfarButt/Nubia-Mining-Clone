4513f9f5a9c2e318d07795ac722b7de31478bcb0
AsfarButt <asfarbutt69@gmail.com>
2025-11-21 21:57:22 +0500

    Update 15

--- app/header.tsx
+++ app/header.tsx
@@ -15,7 +15,7 @@ export default function Header(){
         <div className="relative w-auto h-full flex flex-row items-center justify-center gap-2 scale-80 sm:scale-100">
             {/* <div className="relative h-full w-13 scale-85 rotate-y-180 bg-white mask-[url(/pickaxe.png)] mask-cover mask-center" />
             <h1 className={`relative text-2xl text-white ${cormorant.className}`}>Nubia Mining</h1> */}
-            <div className="relative w-32 h-28 mt-2 bg-[url('/brandtext.png')] bg-center bg-cover"></div>
+            <div className="relative w-30 sm:w-32 h-28 mt-2 bg-[url('/brandtext.png')] bg-center bg-cover"></div>
         </div>
         <div className="relative w-[70%] md:w-[30%] h-full flex flex-row items-center justify-between">
             <a className="relative w-full h-full list-none text-gray-200/90 flex items-center font-sans hover:text-yellow-500/85 justify-center px-1" href="/Home">Home</a>
