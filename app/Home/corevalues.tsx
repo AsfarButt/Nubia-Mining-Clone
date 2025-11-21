261d3d2e503a130866062ce1a4eb233300369620
AsfarButt <asfarbutt69@gmail.com>
2025-11-21 22:02:39 +0500

    Update 16

--- app/Home/corevalues.tsx
+++ app/Home/corevalues.tsx
@@ -28,19 +28,52 @@ export default function CoreValues(){
         }
     },[])
     
-    return(<div className="relative w-full pb-12 h-fit flex items-start justify-center flex-row flex-wrap bg-white">
-                <h1 className="relative w-full py-4 text-[36px] text-black font-sans font-bold text-center underline decoration-red-600/80">Core Values</h1>
-                <div className={`relative w-full h-full max-w-120 transition-all delay-50 translate-y-30 opacity-0 duration-800 ease-out p-4 rounded-md border m-4 border-black/20 shadow-md shadow-transparent hover:shadow-black/30`} ref={element1}>
-                    <h1 className="w-full text-center text-yellow-800/90 text-[24px] font-semibold font-sans">Responsible Development & Innovation</h1>
-                    <p className="text-[17px] px-4 mt-4 pb-8 text-center text-blue-950/85">We pursue advanced exploration and mining using cutting-edge, low impact technologies aligned with JORC principles, ensuring efficiency, safety , and envirnomental reponsibility</p>
-                </div>
-                <div className={`relative w-full h-full max-w-120 transition-all delay-150 translate-y-30 opacity-0 duration-800 ease-out p-4 rounded-md border m-4 border-black/20 shadow-md shadow-transparent hover:shadow-black/30`} ref={element2}>
-                    <h1 className="w-full text-center text-yellow-800/90 text-[24px] font-semibold font-sans">Sustainability & Ethical Sourcing</h1>
-                    <p className="text-[18px] px-4 mt-4 pb-8 text-center text-blue-950/85">We uphold ESG-driven practices and golbal standards for responsible trade -- ensuring compliance, ethical sourcing, and positive community impact across every operation.</p>
-                </div>
-                <div className={`relative w-full h-full max-w-120 transition-all delay-250 translate-y-30 opacity-0 duration-800 ease-out p-4 rounded-md border m-4 border-black/20 shadow-md shadow-transparent hover:shadow-black/30`} ref={element3}>
-                    <h1 className="w-full text-center text-yellow-800/90 text-[24px] font-semibold font-sans">Transparency & Traceability</h1>
-                    <p className="text-[18px] px-4 mt-4 pb-8 text-center text-blue-950/85">We ensure full visibility, integritym and accountability across the value chain through digital monitoring and transparent supply-chain governance.</p>
-                </div>
-        </div>)
+return (
+  <div className="relative w-full pb-12 h-fit flex flex-col items-center bg-white">
+    <h1 className="relative w-full py-4 text-[36px] text-black font-sans font-bold text-center underline decoration-red-600/80">
+      Core Values
+    </h1>
+
+    {/* Row container */}
+    <div className="relative w-full flex flex-col md:flex-row flex-wrap items-stretch justify-center">
+      
+      <div
+        className="relative w-full md:w-[33%] flex-1 transition-all delay-50 translate-y-30 opacity-0 duration-800 ease-out p-4 rounded-md border m-4 border-black/20 shadow-md shadow-transparent hover:shadow-black/30 flex flex-col"
+        ref={element1}
+      >
+        <h1 className="w-full text-center text-yellow-800/90 text-[24px] font-semibold font-sans">
+          Responsible Development & Innovation
+        </h1>
+        <p className="text-[17px] px-4 mt-4 pb-8 text-center text-blue-950/85 flex-1">
+          We pursue advanced exploration and mining using cutting-edge, low impact technologies aligned with JORC principles, ensuring efficiency, safety , and environmental responsibility
+        </p>
+      </div>
+
+      <div
+        className="relative w-full md:w-[33%] flex-1 transition-all delay-150 translate-y-30 opacity-0 duration-800 ease-out p-4 rounded-md border m-4 border-black/20 shadow-md shadow-transparent hover:shadow-black/30 flex flex-col"
+        ref={element2}
+      >
+        <h1 className="w-full text-center text-yellow-800/90 text-[24px] font-semibold font-sans">
+          Sustainability & Ethical Sourcing
+        </h1>
+        <p className="text-[18px] px-4 mt-4 pb-8 text-center text-blue-950/85 flex-1">
+          We uphold ESG-driven practices and global standards for responsible trade -- ensuring compliance, ethical sourcing, and positive community impact across every operation.
+        </p>
+      </div>
+
+      <div
+        className="relative w-full md:w-[33%] flex-1 transition-all delay-250 translate-y-30 opacity-0 duration-800 ease-out p-4 rounded-md border m-4 border-black/20 shadow-md shadow-transparent hover:shadow-black/30 flex flex-col"
+        ref={element3}
+      >
+        <h1 className="w-full text-center text-yellow-800/90 text-[24px] font-semibold font-sans">
+          Transparency & Traceability
+        </h1>
+        <p className="text-[18px] px-4 mt-4 pb-8 text-center text-blue-950/85 flex-1">
+          We ensure full visibility, integrity, and accountability across the value chain through digital monitoring and transparent supply-chain governance.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
 }
--- app/header.tsx
+++ app/header.tsx
@@ -11,21 +11,21 @@ const cormorant = Vend_Sans({
 export default function Header(){
 
 
-    return(<div className="sticky top-0 z-5 w-full h-18 bg-black flex items-center justify-between px-2 md:px-4 before:absolute before:inset-0 before:bg-linear-to-r before:from-blue-950/90 before:to-blue-900/85 overflow-hidden">
+    return(<div className="sticky top-0 z-5 w-full h-18 bg-black flex items-center justify-between px-2 md:px-4 before:absolute before:inset-0 before:bg-linear-to-r before:from-blue-950/90 before:to-blue-900/85">
         <div className="relative w-auto h-full flex flex-row items-center justify-center gap-2 scale-80 sm:scale-100">
             {/* <div className="relative h-full w-13 scale-85 rotate-y-180 bg-white mask-[url(/pickaxe.png)] mask-cover mask-center" />
             <h1 className={`relative text-2xl text-white ${cormorant.className}`}>Nubia Mining</h1> */}
             <div className="relative w-30 sm:w-32 h-28 mt-2 bg-[url('/brandtext.png')] bg-center bg-cover"></div>
         </div>
-        <div className="relative w-[70%] md:w-[30%] h-full flex flex-row items-center justify-between">
+        <div className="relative w-[70%] md:w-[30%] h-full flex flex-row items-center justify-between z-2">
             <a className="relative w-full h-full list-none text-gray-200/90 flex items-center font-sans hover:text-yellow-500/85 justify-center px-1" href="/Home">Home</a>
             <div className="w-1.5 h-[85%] bg-white/60" ></div>
             <a className="relative w-full h-full list-none text-gray-200/90 flex items-center font-sans hover:text-yellow-500/85 justify-center px-1 group" href="/About">About us
             <div className="absolute top-14 p-2 border-l border-white/80 left-0 h-auto w-auto min-w-35 bg-blue-900/60 hidden group-hover:block pointer-events-auto z-1">
-            <><li className="text-white/65 hover:text-white/75 hover:underline decoration-white/70">About Nubia</li>
-            <li className="text-white/65 hover:text-white/75 hover:underline decoration-white/70">Mission</li>
-            <li className="text-white/65 hover:text-white/75 hover:underline decoration-white/70">Logistics</li>
-            <li className="text-white/65 hover:text-white/75 hover:underline decoration-white/70">Our Team</li></>
+            <><li className="text-white/65 p-1 hover:text-white/75 hover:underline decoration-white/70">About Nubia</li>
+            <li className="text-white/65 p-1 hover:text-white/75 hover:underline decoration-white/70">Mission</li>
+            <li className="text-white/65 p-1 hover:text-white/75 hover:underline decoration-white/70">Logistics</li>
+            <li className="text-white/65 p-1 hover:text-white/75 hover:underline decoration-white/70">Our Team</li></>
             </div>
             </a>
             <div className="w-1.5 h-[85%] bg-white/60" ></div>
