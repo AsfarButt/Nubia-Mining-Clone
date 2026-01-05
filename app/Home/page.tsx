5aa72b8e0eb3b7576c60cf86dc8686621009a374
AsfarButt <asfarbutt69@gmail.com>
2026-01-06 04:02:34 +0500

    Update n32153

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -94,7 +94,7 @@ export default function Page(){
         {/* Third Section */}
         <div className="relative flex flex-col md:flex-row items-center justify-center">
             <div className="parentelement relative w-full h-140 before:absolute before:inset-0 before:bg-[url('/sectionleftimage.jpg')] before:bg-center before:opacity-80 before:brightness-120 before:bg-cover flex items-center justify-center">
-                <div className="childelement relative w-[75%] max-w-120 h-auto py-6 group bg-white/50 p-4 hover:bg-white/75 transition-all duration-300 flex flex-col items-center justify-center">
+                <div className="childelement relative w-[75%] max-w-120 h-auto py-6 group bg-white/70 p-4 hover:bg-white/90 transition-all duration-300 flex flex-col items-center justify-center">
                     <h1 className="text-[28px] mt-2 font-semibold text-blue-900/85 text-center">Nubia Mining Services</h1>
                     <h2 className="text-center font-bold font-sans text-black/95 text-[18px]">
                         Tailored solutions at every stage of mining operations, designed to maximize efficiency, safety, and sustainability.
@@ -104,8 +104,8 @@ export default function Page(){
                     </a>
                 </div>
             </div>
-            <div className="parentelement relative w-full h-140 before:absolute before:inset-0 before:bg-[url('/sectionrightimage.avif')] before:bg-center before:opacity-80 before:brightness-120 before:bg-cover flex items-center justify-center">
-                <div className="childelement relative w-[75%] max-w-120 h-auto py-6 group bg-white/50 p-4 hover:bg-white/75 transition-all duration-300 flex flex-col items-center justify-center">
+            <div className="parentelement relative w-full h-140 before:absolute before:inset-0 before:bg-[url('/sectionrightimage.jpg')] before:bg-center before:opacity-80 before:brightness-120 before:bg-cover flex items-center justify-center">
+                <div className="childelement relative w-[75%] max-w-120 h-auto py-6 group bg-white/70 p-4 hover:bg-white/90 transition-all duration-300 flex flex-col items-center justify-center">
                     <h1 className="text-[28px] mt-2 font-semibold text-blue-900/85 text-center">Nubia Mining Products</h1>
                     <h2 className="text-center font-bold font-sans text-black/95 text-[18px]">
                         High-quality minerals and raw materials, responsibly sourced from our mines and delivered to meet modern industrial demands.
